35dd7879812f8ceca09ca0aa977c0be6971771f1
Laila Kandil <101605144+LailaKandil@users.noreply.github.com>
2024-05-03 20:56:28 +0300

    almost done

--- Question 1-Uninformed Search.pl
+++ Question 1-Uninformed Search.pl
@@ -1,30 +1,31 @@
+%Request Grid Size from user
 grid_size(N, M) :-
     write('Enter the number of rows (n): '),
     read(N),
     write('Enter the number of columns (m): '),
     read(M).
 
-create_row(0,_,_, Row, Row).
-create_row(M, N,Temp, Acc, Row) :-
-    M > 0,
-    format('Enter a color for position (~w, ~w) (red/yellow/blue): ', [N, Temp]),
-    read(Color),
-    member(Color, [red, yellow, blue]),
-    append(Acc, [Color], NewAcc),
-    M1 is M - 1,
-    Temp1 is Temp+1,
-    create_row(M1, N,Temp1, NewAcc, Row).
-
 create_grid(N, M, Grid) :-
-    create_grid(N, M, 1, [], Grid).
+    create_grid(N, M, [], Grid).
 
-create_grid(0, _, _, Grid, Grid).
-create_grid(N, M, RowNum, Acc, Grid) :-
-    create_row(M, RowNum,1, [], Row),
-    append(Acc, [Row], NewAcc),
+create_grid(0, _, Grid, Grid).
+create_grid(N, M, Acc, Grid) :-
+    create_row(N,M, [], Row),
+    append(Row,Acc, NewAcc),
     N1 is N - 1,
-    RowNum1 is RowNum + 1,
-    create_grid(N1, M, RowNum1, NewAcc, Grid).
+    create_grid(N1, M, NewAcc, Grid).
+
+create_row(_,0, Row, Row).
+create_row(CurrentRow,M, Acc, Row) :-
+    CurrentRow > 0,
+    M > 0,
+    format('Enter a color for position (~w, ~w) (red/yellow/blue): ', [CurrentRow, M]),
+    read(Color),
+    (Color = red ; Color = yellow; Color = blue),
+    append([(CurrentRow,M,Color)],Acc, NewAcc),
+    M1 is M - 1,
+    create_row(CurrentRow,M1, NewAcc, Row).
+
 
 print_grid([]).
 print_grid([Row|Rows]) :-
@@ -34,47 +35,54 @@ print_grid([Row|Rows]) :-
 
 print_row([], _).
 print_row([Color|Colors], Position) :-
-    write('( ')
+    write('('),
     write(Color),
     write(') '),
     NextPosition is Position + 1,
     print_row(Colors, NextPosition).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Define valid moves within the grid bounds
-% Define valid moves within the grid bounds
-move((X1, Y1, Color), (X2, Y2, Color), N, M) :-
-    X2 is X1 + 1, X2 < N, Y2 is Y1, Y2 < M.
-move((X1, Y1, Color), (X2, Y2, Color), _, _) :-
-    X2 is X1 - 1, X2 >= 0, Y2 is Y1.
-move((X1, Y1, Color), (X2, Y2, Color), _, M) :-
-    X2 is X1, X2 < N, Y2 is Y1 + 1, Y2 < M.
-move((X1, Y1, Color), (X2, Y2, Color), N, _) :-
-    X2 is X1, X2 < N, Y2 is Y1 - 1, Y2 >= 0.
+% Finding the cycle
+% Define valid moves within the grid bound
+% Define the moves
+move((X,Y,CurrentNodeColor), (NX, Y, CurrentNodeColor)) :- NX is X + 1. % Move down
+move((X,Y,CurrentNodeColor), (NX, Y, CurrentNodeColor)) :- NX is X - 1. % Move up
+move((X,Y,CurrentNodeColor), (X,NY,CurrentNodeColor)) :- NY is Y - 1. % Move left
+%move([X, Y], [NX, Y]) :- NX is X - 1. % Move up
 
-% Depth-first search to find color cycle
-search(_, [], _, _, _) :- fail. % No cycle found
-search(CurrentState, Visited, N, M, Goal):-
-    member(CurrentState, Visited),
-    write("Cycle found!"), nl,
-    write(Visited), nl, write(CurrentState).
-search(CurrentState, Visited, N, M, Goal):-
-    move(CurrentState, NextState, N, M),
+move((X,Y,CurrentNodeColor), (X, NY, CurrentNodeColor)) :- NY is Y + 1. % Move right
+%move([X, Y], [X, NY]) :- NY is Y - 1. % Move left
+
+
+% Depth-first search to find color cycle meeting specified conditions
+search(CurrentState, Grid, Visited, Path, Parent) :-
+    move(CurrentState, NextState),
+    not(member(NextState, Parent)), % replace with not equal
+    member(NextState, Grid), % is a valid next state
+    member(NextState, Visited),
+	write("Search is complete!"), nl,
+    write(Path), nl.
+
+search(CurrentState, Grid, Visited, Path, Parent) :-
+    move(CurrentState, NextState),
+    not(member(NextState, Parent)), % replace with not equal
+    member(NextState, Grid),
     not(member(NextState, Visited)),
-    append(Visited, [CurrentState], NewVisited),
-    search(NextState, NewVisited, N, M, Goal).
+    append(Visited, [NextState], NewVisited),
+    append(Path, [NextState], NewPath),
+    search(NextState, Grid, NewVisited, NewPath, [CurrentState]).
 
-% Start the search for color cycle
-find_color_cycle(InitialColor, N, M) :-
-    search((0, 0, InitialColor), [], N, M, (N, M, InitialColor)).
+trySearchStartPoint([],_).
+trySearchStartPoint([FirstNode|Nodes], Grid) :-
+    %write(Nodes), nl,
+    trySearchStartPoint(Nodes, Grid),
+    search(FirstNode, Grid, [FirstNode], [FirstNode], []).
 
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-main :-
+%Main Predicate
+main:-
     grid_size(N, M),
     create_grid(N, M, Grid),
-    print_grid(Grid),
-    %find_color_cycle(red, N, M),
+    %print_grid(Grid),
+    write('Done.'),
+    trySearchStartPoint(Grid, Grid),
     writeln('Done.').
